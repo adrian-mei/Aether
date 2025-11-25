b45571f6e1b0476d813f54b997b147ef484473b7
Adrian <adrian.z.mei@gmail.com>
2025-11-25 03:48:46 -0800

    feat: Proxy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Disables the X-Powered-By header for security and cleaner headers
   poweredByHeader: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/backend/:path*',
+        destination: 'http://159.54.180.60:3002/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
